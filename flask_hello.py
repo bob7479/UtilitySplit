01defa4272e4b5125d4dd4a5021866197c8019dd
Daniel Shi <macbook@airbears2-10-142-157-15.airbears2.1918.berkeley.edu>
2016-11-12 00:50:58 -0800

    I'm trying :'(

--- flask_hello.py
+++ flask_hello.py
@@ -1,17 +1,21 @@
-from flask import Flask, url_for
+from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def index(): pass
+def index():
+    return 'Index Page'
 
-@app.route('/login')
-def login(): pass
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
 
 @app.route('/user/<username>')
-def profile(username): pass
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
 
-with app.test_request_context():
-	print url_for('index')
-	print url_for('login')
-	print url_for('login', next='/')
-	print url_for('profile', username='bob7479')
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
