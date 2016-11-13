6b8564d5870a3b91d7bf25fe1fd3168156ab2109
Daniel Shi <macbook@airbears2-10-142-157-15.airbears2.1918.berkeley.edu>
2016-11-12 17:50:15 -0800

    add_bill not displaying

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -67,15 +67,15 @@ def show_entries():
 def add_bill():
     if not session.get('logged_in'):
         abort(401)
-    if request.method == 'POST':
+    if request.method == 'GET':
         db = get_db()
         db.execute('insert into bills (billname, category, frequency, cost) values (?, ?, ?, ?)',
                [request.form['billname'], request.form['category'], \
                request.form['frequency'], request.form['cost']])
         db.commit()
         flash('New entry was successfully posted')
-        return redirect(url_for('show_bills'))
-    return redirect(url_for('show_bills'))
+        return redirect(url_for('show_entries'))
+    return redirect(url_for('show_entries'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -88,7 +88,7 @@ def login():
         else:
             session['logged_in'] = True
             flash('You were logged in')
-            return redirect(url_for('show_entries'))
+            return redirect(url_for('add_bill'))
     return render_template('login.html', error=error)
 
 
