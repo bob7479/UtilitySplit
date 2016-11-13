afc4e0ab7437319e61472c2d93194a715a524116
Nanxi Wang <nanxiwang9@gmail.com>
2016-11-12 17:43:37 -0800

    struggling

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -63,17 +63,19 @@ def show_entries():
     print(entries)
     return render_template('show_entries.html', entries=entries)
 
-@app.route('/add_bill', methods=['POST'])
+@app.route('/add_bill', methods=['GET','POST'])
 def add_bill():
     if not session.get('logged_in'):
         abort(401)
-    db = get_db()
-    db.execute('insert into bills (billname, category, frequency, cost) values (?, ?, ?, ?)',
+    if request.method == 'POST':
+        db = get_db()
+        db.execute('insert into bills (billname, category, frequency, cost) values (?, ?, ?, ?)',
                [request.form['billname'], request.form['category'], \
                request.form['frequency'], request.form['cost']])
-    db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
+        db.commit()
+        flash('New entry was successfully posted')
+        return redirect(url_for('show_bills'))
+    return redirect(url_for('show_bills'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
