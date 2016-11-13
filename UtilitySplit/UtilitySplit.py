ecd97728c763829af53f8874844ec22af0a90977
Daniel Shi <macbook@airbears2-10-142-156-243.airbears2.1918.berkeley.edu>
2016-11-12 22:10:46 -0800

    renamed

--- UtilitySplit/UtilitySplit.py
+++ UtilitySplit/UtilitySplit.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    DATABASE=os.path.join(app.root_path, 'UtilitySplit.db'),
     DEBUG=True,
     SECRET_KEY='development key',
     USERNAME='admin',
     PASSWORD='default'
 
 ))
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+app.config.from_envvar('UTILITYSPLIT_SETTINGS', silent=True)
 
 def connect_db():
     """Connects to the specific database."""
